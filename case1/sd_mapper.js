83767792773bd66d71f6ee7be8dfe64537a8463e
Kevin <kramirez234@west-mec.org>
2020-01-21 16:29:31 -0700

    Done with case 1 and 2
    I by accident made a mistake in cloning a repo so i copied and pasted all of my work in my new files and these are my new files that i will turn in.

---
+++ case1/sd_mapper.js
@@ -0,0 +1,23 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: Kevin Ramirez
+   Date:   1-21-20
+
+*/
+
+var thisTime = new Date();
+var timeStr = thisTime.toLocaleString();
+
+document.getElementById("timeStamp").innerHTML = timeStr;
+
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+
+var mapNum = (2 * thisMonth + thisHour) % 24;
+var imgStr = "<img src='sd_sky" + mapNum +  ".png'/>";
+
+document.getElementById("planisphere").insertAdjacentHTML("afterbegin", imgStr);
--- case2/bc_today.js
+++ case2/bc_today.js
@@ -1,12 +1,12 @@
-
+"use strict"
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 2
 
    Today at the Union Script
-   Author: 
-   Date:   
+   Author: Kevin Ramirez
+   Date:   1-21-20
    
    This script uses the getEvent() function to return the
    HTML code containin the daily events at the Bridger College
@@ -16,6 +16,14 @@
 
 
 
+var thisDate = new Date();
+var dateString = thisDate.toLocaleDateString();
+var dateHTML = "<h2>" + dateString + "</h2>";
+var thisDay = thisDate.getDay(); 
+
+var eventHTML = getEvent(thisDay);
+document.getElementById("unionToday").insertAdjacentHTML("beforeend", dateHTML + eventHTML);
+ 
 function getEvent(day) {
    var eventHTML;
    switch (day) {
